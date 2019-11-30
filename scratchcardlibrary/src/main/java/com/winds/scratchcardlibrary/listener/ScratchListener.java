a2dfaf042183f3b0f9539ef7ae4bc4c412a5a2c3
kaju02525 <karunkumar02525@gmail.com>
2019-11-30 20:07:22 +0530

    scrach card to win

---
+++ scratchcardlibrary/src/main/java/com/winds/scratchcardlibrary/listener/ScratchListener.java
@@ -0,0 +1,27 @@
+package com.winds.scratchcardlibrary.listener;
+
+
+import com.winds.scratchcardlibrary.ui.ScratchCardLayout;
+
+/**
+ * Interface to receive scratch related callbacks
+ */
+public interface ScratchListener {
+    /**
+     * Callback indicating Scratch started in the UI
+     */
+    void onScratchStarted();
+
+    /**
+     * Callback indicating progress percentage of scratch
+     * @param scratchCardLayout the scratch card layout itself
+     * @param atLeastScratchedPercent percentage that's at least scratched at this point
+     *                                (not providing accurate values in real-time due to performance reasons)
+     */
+    void onScratchProgress(ScratchCardLayout scratchCardLayout, int atLeastScratchedPercent);
+
+    /**
+     * Callback indicating Scratch stopped/interrupted in the UI
+     */
+    void onScratchComplete();
+}
